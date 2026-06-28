97193ac92038dbe9d92a6a603d1b2c9f12a7e8e3
houtan <houtan.rocky@gmail.com>
2026-06-28 06:12:38 +0330

    test: add tests for item service

---
+++ internal/item/errors.go
@@ -0,0 +1,5 @@
+package item
+
+import "errors"
+
+var ErrItemNotFound = errors.New("item not found")
---
+++ internal/item/service_test.go
@@ -0,0 +1,101 @@
+package item
+
+import (
+	"context"
+	"testing"
+)
+
+type MockItemRepository struct {
+	items map[string]*Item
+}
+
+func (r MockItemRepository) GetByID(ctx context.Context, id string) (*Item, error) {
+	i, ok := r.items[id]
+	if !ok {
+		return nil, ErrItemNotFound
+	}
+	return i, nil
+}
+
+func (r MockItemRepository) Update(ctx context.Context, i *Item) error {
+	r.items[i.ID] = i
+	return nil
+}
+
+func (r MockItemRepository) ListAvailable(ctx context.Context) ([]*Item, error) {
+	var result []*Item
+	for _, it := range r.items {
+		if it.Available {
+			result = append(result, it)
+		}
+	}
+	return result, nil
+}
+
+// ---------------------------- Tests ----------
+func TestService_Get_Success(t *testing.T) {
+	const (
+		testInitialItemID    = "item-1"
+		testInitialItemName  = "Sussy Sword"
+		testInitialItemType  = Common
+		testInitialOwnerId   = "guild-1"
+		testInitialAvailable = true
+		testInitialBasePrice = 1000
+	)
+	repo := &MockItemRepository{items: map[string]*Item{
+		testInitialItemID: {
+			ID:        testInitialItemID,
+			Name:      testInitialItemName,
+			Type:      testInitialItemType,
+			OwnerID:   testInitialOwnerId,
+			Available: testInitialAvailable,
+			BasePrice: testInitialBasePrice,
+		},
+	}}
+
+	ctx := context.Background()
+	svc := NewItemService(repo)
+
+	i, err := svc.Get(ctx, testInitialItemID)
+	if err != nil {
+		t.Error(err)
+	}
+	if i.ID != testInitialItemID || i.Name != testInitialItemName {
+		t.Errorf("unexpected item %v", i)
+	}
+}
+
+func TestService_ListAvailable(t *testing.T) {
+	// -------- Arrange ---------------------------
+	initialItems := map[string]*Item{
+		"item-1": {
+			ID:        "item-1",
+			Available: true,
+		},
+		"item-2": {
+			ID:        "item-2",
+			Available: true,
+		},
+	}
+
+	repo := &MockItemRepository{items: initialItems}
+	ctx := context.Background()
+	svc := NewItemService(repo)
+
+	// -------- Act ------------
+	items, err := svc.ListAvailable(ctx)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	// -------- Assert ----------
+	if len(items) != 2 {
+		t.Errorf("expected 2 items, got %v", len(items))
+	}
+	for _, it := range items {
+		if it.Available != true {
+			t.Errorf("returned item %v is not available", it.ID)
+		}
+	}
+
+}
